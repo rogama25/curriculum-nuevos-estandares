a03d1d7ba313cb701dabdbfc72c50c7195def28c
Rodrigo Garcia Martin <rodrigogarciama@gmail.com>
2023-01-01 13:54:06 +0100

    fin version pc

--- js/common.js
+++ js/common.js
@@ -3,6 +3,10 @@ function modalAssistant() {
     var elem = document.getElementById("modalAssistant")
     elem.style.display = "flex";
     document.body.style.overflow = "hidden";
+    elem.classList.add("scale-up-center")
+    setTimeout(() => {
+        elem.classList.remove("scale-up-center")
+    }, 400);
 }
 
 function modalSensors() {
@@ -10,18 +14,30 @@ function modalSensors() {
     var elem = document.getElementById("modalSensors")
     elem.style.display = "flex";
     document.body.style.overflow = "hidden";
+    elem.classList.add("scale-up-center")
+    setTimeout(() => {
+        elem.classList.remove("scale-up-center")
+    }, 400);
 }
 
 function modalCloseAssistant() {
     event.preventDefault();
     var elem = document.getElementById("modalAssistant")
-    elem.style.display = "none";
     document.body.style.overflow = "auto";
+    elem.classList.add("scale-down-center")
+    setTimeout(() => {
+        elem.classList.remove("scale-down-center")
+        elem.style.display = "none";
+    }, 400);
 }
 
 function modalCloseSensors() {
     event.preventDefault();
     var elem = document.getElementById("modalSensors")
-    elem.style.display = "none";
     document.body.style.overflow = "auto";
+    elem.classList.add("scale-down-center")
+    setTimeout(() => {
+        elem.classList.remove("scale-down-center")
+        elem.style.display = "none";
+    }, 400);
 }
